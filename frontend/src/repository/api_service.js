5c017d4fb31d05e757cffcb4e7fb21e456c525c1
yousefAiSoftware <computer.m2020@gmail.com>
2025-10-24 23:12:48 +0300

    Modified API URL

--- frontend/src/repository/api_service.js
+++ frontend/src/repository/api_service.js
@@ -1,7 +1,7 @@
 import axios from  'axios';
 
 
-const  API_BASE_URL = 'http://localhost:5000/api';
+const  API_BASE_URL = 'https://todo-app-3kcj.onrender.com/api';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
